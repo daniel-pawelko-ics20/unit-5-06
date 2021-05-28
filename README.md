55dc771e13e4bf40d1852dc4e16780846185b48d
danielpawelkoic <>
2021-05-28 23:54:09 +0000

    repl is lagging really badly

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # unit-5-06
+[![GitHub's Super Linter](https://github.com/daniel-pawelko-ics20/unit-5-06/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/daniel-pawelko-ics20/unit-5-06/actions)
+
+
+
+This website can be found at: [https://daniel-pawelko-ics20.github.io/unit-5-06](https://daniel-pawelko-ics20.github.io/unit-5-06)
---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2021 Daniel Pawelko All rights reserved
+//
+// Created by: Daniel Pawelko
+// Created on: May 2021
+// This file contains the JS functions for index.html
+
+"use strict"
+
+
+// Defining function that actives when user presses "Check" Button
+function userInputClick() {
+  // Store input/requirements in variables
+  const first = parseInt(document.getElementById("number1-entered").value)
+  const second = parseInt(document.getElementById("number2-entered").value)
+  let counter = 0;
+  let final = 0;
+
+  // Performs calculation
+  while (counter != second) {
+    final += first
+    counter++
+    console.log(final)
+  }
+}
